338a3aa26de9f0a8626986115bac840defe2e385
Dewi Williams <dewi@dewiwilliams.co.uk>
2022-12-18 20:32:47 +0000

    Day 18 part 2

--- day18/main.go
+++ day18/main.go
@@ -9,6 +9,12 @@ import (
 	"strings"
 )
 
+/*
+ * I've taken the approach here of cramming {x, y, z} in to int by some bit-shifting: x | (y<<8) | (z<<16)
+ * Not sure I like that approach - it made some things easier, but meant my values weren't readable, and meant
+ * that I had to move my data set a little ot make sure I was always dealing with positive integers.
+ */
+
 type cube struct {
 	id int
 	x  int
@@ -34,10 +40,13 @@ func (c cube) adjacentTo(c1 cube) bool {
 func main() {
 	data := getData("./input.txt")
 
-	fmt.Printf("Got data: %v\n", data)
 	fmt.Printf("Part 1: %d\n", part1(data))
+	fmt.Printf("Part 2: %d\n", part2(data))
 }
 func part1(cubes []cube) int {
+	return getTotalExposedFaces(cubes)
+}
+func getTotalExposedFaces(cubes []cube) int {
 	result := 0
 
 	for _, c := range cubes {
@@ -45,6 +54,145 @@ func part1(cubes []cube) int {
 	}
 
 	return result
+}
+
+func part2(cubes []cube) int {
+	bounds := getBounds(cubes)
+
+	bounds[0]--
+	bounds[1]++
+	bounds[2]--
+	bounds[3]++
+	bounds[4]--
+	bounds[5]++
+
+	toProcess := []int{toInt(bounds[0], bounds[2], bounds[4])}
+	water := []int{}
+	intCubes := convertCubesToIntCube(cubes)
+
+	for len(toProcess) > 0 {
+		currentCell := toProcess[len(toProcess)-1]
+		toProcess = toProcess[:len(toProcess)-1]
+
+		water = append(water, currentCell)
+
+		neighbours := getNeighbours(currentCell)
+		for _, n := range neighbours {
+			if !isInBounds(n, bounds) {
+				continue
+			}
+			if contains(intCubes, n) || contains(water, n) || contains(toProcess, n) {
+				continue
+			}
+			toProcess = append(toProcess, n)
+		}
+	}
+
+	filledGrid := fillEmptyHoles(bounds, water, cubes, intCubes)
+
+	return getTotalExposedFaces(filledGrid)
+}
+func fillEmptyHoles(bounds []int, water []int, cubes []cube, intcubes []int) []cube {
+	result := make([]cube, len(cubes))
+	copy(result, cubes)
+
+	for x := bounds[0]; x <= bounds[1]; x++ {
+		for y := bounds[2]; y <= bounds[3]; y++ {
+			for z := bounds[4]; z <= bounds[5]; z++ {
+
+				value := toInt(x, y, z)
+				if contains(water, value) || contains(intcubes, value) {
+					continue
+				}
+
+				result = append(result, cube{id: len(result), x: x, y: y, z: z})
+			}
+		}
+	}
+
+	return result
+}
+func toIntCube(c cube) int {
+	return toInt(c.x, c.y, c.z)
+}
+func toInt(x, y, z int) int {
+	return x | y<<8 | z<<16
+}
+func getNeighbours(cell int) []int {
+	return []int{
+		cell + 1,
+		cell - 1,
+		cell + (1 << 8),
+		cell - (1 << 8),
+		cell + (1 << 16),
+		cell - (1 << 16),
+	}
+}
+func isInBounds(cell int, bounds []int) bool {
+
+	x := cell & 0xFF
+	y := (cell >> 8) & 0xFF
+	z := (cell >> 16) & 0xFF
+
+	return x >= bounds[0] && x <= bounds[1] && y >= bounds[2] && y <= bounds[3] && z >= bounds[4] && z <= bounds[5]
+}
+func convertCubesToIntCube(cubes []cube) []int {
+	result := []int{}
+
+	for _, c := range cubes {
+		result = append(result, toIntCube(c))
+	}
+
+	return result
+}
+func contains(haystack []int, needle int) bool {
+
+	for _, item := range haystack {
+		if item == needle {
+			return true
+		}
+	}
+
+	return false
+}
+func getBounds(cubes []cube) []int {
+
+	base := 1<<8 - 1
+
+	minX := base - 1
+	maxX := 0
+	minY := base - 1
+	maxY := 0
+	minZ := base - 1
+	maxZ := 0
+
+	for _, c := range cubes {
+		if c.x < 0 || c.x >= base || c.y < 0 || c.y >= base || c.z < 0 || c.z >= base {
+			fmt.Println("Out of bounds!")
+			os.Exit(1)
+		}
+
+		if c.x > maxX {
+			maxX = c.x
+		}
+		if c.x < minX {
+			minX = c.x
+		}
+		if c.y > maxY {
+			maxY = c.y
+		}
+		if c.y < minY {
+			minY = c.y
+		}
+		if c.z > maxZ {
+			maxZ = c.z
+		}
+		if c.z < minZ {
+			minZ = c.z
+		}
+	}
+
+	return []int{minX, maxX, minY, maxY, minZ, maxZ}
 }
 func getExposedFacesForCube(target cube, cubes []cube) int {
 	result := 6
@@ -88,9 +236,9 @@ func getData(filename string) []cube {
 		z, _ := strconv.Atoi(parts[2])
 		c := cube{
 			id: lineIndex,
-			x:  x,
-			y:  y,
-			z:  z,
+			x:  x + 16,
+			y:  y + 16,
+			z:  z + 16,
 		}
 		result = append(result, c)
 
